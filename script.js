1c08cef7db72080ed8c9b259f7f99cb58ed563f6
Sameer <tenyson2005@gmail.com>
2023-05-25 20:23:39 +0530

    everything working

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById('click').addEventListener('click', function () {
     console.log("button has been clicked");
     game();
+    
 })
-
+    var scoreP=0;
+    var scoreC=0;
 //main logic
 function game() {
     let inp = ["rock", "paper", "scissor"];
@@ -15,43 +17,64 @@ function game() {
     let playerchoice = inp[y];
     playerImg(y);
 
+    
+
     if (playerchoice == compchoice) {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("its a tie");
         document.getElementById('status').innerText = "It's a tie!";
+        
     }
     //paper
     else if (playerchoice == "paper" && compchoice == "scissor") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("computer wins");
         document.getElementById('status').innerText = 'Computer won';
+
+        scoreC++;
+        document.getElementById('score-computer').innerText= scoreC;
     }
     else if (playerchoice == "paper" && compchoice == "rock") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("player wins");
         document.getElementById('status').innerText = 'Player won';
+
+        scoreP++
+        document.getElementById('score-player').innerText = scoreP;
     }
     //rock
     else if (playerchoice == "rock" && compchoice == "scissor") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("player wins");
         document.getElementById('status').innerText = 'Player won';
+
+        scoreP++
+        document.getElementById('score-player').innerText = scoreP;
     }
     else if (playerchoice == "rock" && compchoice == "paper") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("computer wins");
         document.getElementById('status').innerText = 'Computer won';
+
+        scoreC++;
+        document.getElementById('score-computer').innerText= scoreC;
     }
     //scissor
     else if (playerchoice == "scissor" && compchoice == "paper") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("computer wins");
         document.getElementById('status').innerText = 'Computer won';
+
+        scoreC++;
+        document.getElementById('score-computer').innerText= scoreC;
     }
     else if (playerchoice == "scissor" && compchoice == "rock") {
         console.log("computer got " + compchoice + " and player got " + playerchoice);
         console.log("player wins");
         document.getElementById('status').innerText = 'Player won';
+
+        scoreP++
+        document.getElementById('score-player').innerText = scoreP;
     }
 
 }
